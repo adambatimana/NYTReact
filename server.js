3f6c92dd44f9f393bb8b746ca000a3a7e1453911
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-08-21 14:02:40 -0400

    init files

---
+++ server.js
@@ -0,0 +1,67 @@
+// ==================================
+//            DEPENDANCIES
+// ==================================
+let express = require("express");
+let bodyParser = require("body-parser");
+let logger = require("morgan");
+let mongoose = require("mongoose");
+
+// ==================================
+//            Require SCHEMA
+// ==================================
+
+// ==================================
+//            Express
+// ==================================
+let app = express();
+let PORT = process.env.PORT || 8000;
+
+// ==================================
+//            MORGAN
+// ==================================
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("public"));
+
+// ==================================
+//            MONGODB CONFIG
+// ==================================
+mongoose.connect("mongodb://localhost/nytreact");
+let db = mongoose.connection;
+
+db.on("error", function(err) {
+      console.log("Mongoose Error: " + err);
+});
+
+db.once("open", function () {
+      console.log("MONGOOSE CONNECTION SUCCESS!");
+});
+
+// ==================================
+//            ROUTES
+// ==================================
+
+app.get("/", function(req,res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+
+app.get("/", function(req,res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+
+app.post("/", function(req,res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+// ==================================
+//           PORT LISTENER
+// ==================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
