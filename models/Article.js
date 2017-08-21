af33334259ebe012ce5f33f022266576f1312065
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-08-21 14:06:45 -0400

    schema

---
+++ models/Article.js
@@ -0,0 +1,19 @@
+let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+
+let ArticleSchema = new Schema ({
+      title:{
+            type:String
+      },
+      date: {
+            type:Date
+      },
+      url: {
+            type:String
+      }
+
+});
+
+let Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
