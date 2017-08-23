ebacb87cd38fe099fc87489a3f3bc6a88140949b
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-08-23 13:58:59 -0400

    search js

--- app/components/children/Search.js
+++ app/components/children/Search.js
@@ -7,26 +7,22 @@ let React = require("react");
 //      RESULTS COMPONENT
 // ======================
 let Results = React.createClass({
-        render: function( )
-                        {
-                            return
-                             (
-                                     <div className="card blue-grey darken-1">
-                                            <div className="card-content white-text">
-                                                      <span className="card-title">Results</span>
-                                                        <h2>{this.props.title}</h2>
-                                                        <p>{this.props.date}</p>
-                                                        <button
-                                                            className="btn waves-effect waves-light"
-                                                            name="save"
-                                                        >
-                                                                            SAVE
-                                                        </button>
-                                            </div>
-                                        </div>
-                             );//end return
-                        }//end render
+  render: function() {
+    return (
+      <div className="card blue-grey darken-1">
+        <div className="card-content white-text">
+          <span className="card-title">Results</span>
+          <h2>{this.props.title}</h2>
+          <p>{this.props.date}</p>
+          <p>{this.props.results}</p>
+          <button className="btn waves-effect waves-light" name="save">
+            SAVE
+          </button>
+        </div>
+      </div>
+    ); //end return
+  } //end render
 
-});// end results
-          
+}); // end results
+
 module.exports = Results;
